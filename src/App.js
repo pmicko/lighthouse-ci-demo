31eb086369c1f5f8c8bb30d88ea3128d645e9709
petrmi <petr.micko@gmail.com>
2021-06-25 22:53:02 +0200

    update link

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          React
+          Learn React
         </a>
         <a
             className="App-link"
